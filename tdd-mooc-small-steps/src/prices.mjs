b267bede17a67836cbd76d03c3164c74aa6392cd
Aija Valleala <aija.valleala@solita.fi>
2023-01-01 13:08:27 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -31,6 +31,7 @@ function createApp(database) {
   }
 
   function calculateCost(age, type, date, baseCost, date2) {
+    console.log(date2);
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
