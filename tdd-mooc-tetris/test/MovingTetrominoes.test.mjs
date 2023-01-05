4a79cabfc672bac2359c6fb6d110360585108872
Aija Valleala <aija.valleala@solita.fi>
2023-01-05 10:46:49 +0200

    tests moving falling tetrominoes pass

--- tdd-mooc-tetris/test/MovingTetrominoes.test.mjs
+++ tdd-mooc-tetris/test/MovingTetrominoes.test.mjs
@@ -16,7 +16,7 @@ describe("Falling and moving tetrominoes", () => {
     board = new Board(10, 6);
   });
 
-  
+
   it("starts from the top middle when there are empty rows above the shape", () => {
     board.drop(PLUS_SHAPE);
 
@@ -30,7 +30,7 @@ describe("Falling and moving tetrominoes", () => {
     );
   });
 
-  xit("can be moved left", () => {
+  it("can be moved left", () => {
     board.drop(PLUS_SHAPE);
     board.moveLeft();
 
@@ -44,38 +44,39 @@ describe("Falling and moving tetrominoes", () => {
     );
   });
 
-xit("can be moved rigth", () => {
-  board.drop(Tetromino.T_SHAPE);
-  board.moveRight();
+  it("can be moved rigth", () => {
+    board.drop(PLUS_SHAPE);
+    board.moveRight();
 
-  expect(board.toString()).to.equalShape(
-    `.....T....
-     ....TTT...
-     ..........
+    expect(board.toString()).to.equalShape(
+      `.....X....
+     ....XXX...
+     .....X....
      ..........
      ..........
      ..........`
-  );
-});
+    );
+  });
 
-it("can be moved down", () => {
-  board.drop(Tetromino.T_SHAPE);
-  board.moveDown();
+  it("can be moved down", () => {
+    board.drop(PLUS_SHAPE);
+    board.moveDown();
 
-  expect(board.toString()).to.equalShape(
-    `..........
+    expect(board.toString()).to.equalShape(
+      `..........
      ..........
      ..........
-     ..........
-     ....T.....
-     ...TTT....`
-  );
-});
+     ....X.....
+     ...XXX....
+     ....X.....`
+    );
+  });
 
-// it cannot be moved left beyond the board
-// it cannot be moved right beyond the board
-// it cannot be moved down beyond the board (will stop falling)
-// it cannot be moved left through other blocks
-// it cannot be moved right through other blocks
-// it cannot be moved down through other blocks (will stop falling)
+
+  // it cannot be moved left beyond the board
+  // it cannot be moved right beyond the board
+  // it cannot be moved down beyond the board (will stop falling)
+  // it cannot be moved left through other blocks
+  // it cannot be moved right through other blocks
+  // it cannot be moved down through other blocks (will stop falling)
 })
