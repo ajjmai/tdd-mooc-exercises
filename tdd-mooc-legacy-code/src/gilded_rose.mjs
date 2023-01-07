c7bae8eb09393b6dd4841f3a143b37f3da3e7997
Aija Valleala <aija.valleala@solita.fi>
2023-01-07 16:00:23 +0200

    refactor backstage pass quality after sell in date

--- tdd-mooc-legacy-code/src/gilded_rose.mjs
+++ tdd-mooc-legacy-code/src/gilded_rose.mjs
@@ -51,22 +51,23 @@ export class Shop {
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.decrementSellIn(this.items[i]);
       }
+
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.decrementQualityByOne(this.items[i])
-              }
+        if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+          if (this.items[i].quality > 0) {
+            if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              this.decrementQualityByOne(this.items[i])
             }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
           }
         }
+      }
+
+      if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert" && this.items[i].sellIn < 0) {
+        this.items[i].quality = 0
       }
     }
 
