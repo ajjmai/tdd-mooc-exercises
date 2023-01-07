a71e447810f57412e7e9d82e26571c5d3efa9d6a
Aija Valleala <aija.valleala@solita.fi>
2023-01-07 15:49:55 +0200

    refactor decrement sell in value

--- tdd-mooc-legacy-code/src/gilded_rose.mjs
+++ tdd-mooc-legacy-code/src/gilded_rose.mjs
@@ -11,6 +11,11 @@ export class Shop {
     this.items = items;
   }
 
+
+  decrementSellIn(item) {
+    item.sellIn = item.sellIn - 1;
+  }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
@@ -37,7 +42,7 @@ export class Shop {
         }
       }
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.decrementSellIn(this.items[i]);
       }
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
