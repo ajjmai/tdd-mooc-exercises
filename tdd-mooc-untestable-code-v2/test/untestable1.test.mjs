2a60f01dfea7632cc5a6f6b8ce11f5ee69039192
Aija Valleala <aija.valleala@solita.fi>
2023-02-18 09:35:25 +0200

    refactor and test untestable 1

--- tdd-mooc-untestable-code-v2/test/untestable1.test.mjs
+++ tdd-mooc-untestable-code-v2/test/untestable1.test.mjs
@@ -1,9 +1,24 @@
 import { expect } from "chai";
-import { daysUntilChristmas } from "../src/untestable1.mjs";
+import { daysUntilChristmasRefactored } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  it("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  it("is 0 days from 25.12.", () => {
+    const fromDate = new Date(Date.UTC(2023, 11, 25));
+    expect(daysUntilChristmasRefactored(fromDate)).to.equal(0);
+  });
+
+  it("is 1 day from 24.12.", () => {
+    const fromDate = new Date(Date.UTC(2023, 11, 24));
+    expect(daysUntilChristmasRefactored(fromDate)).to.equal(1);
+  });
+
+  it("is 358 days from 1.1.", () => {
+    const fromDate = new Date(Date.UTC(2023, 0, 1));
+    expect(daysUntilChristmasRefactored(fromDate)).to.equal(358);
+  });
+
+  it("is 364 days from 26.12.", () => {
+    const fromDate = new Date(Date.UTC(2022, 11, 26));
+    expect(daysUntilChristmasRefactored(fromDate)).to.equal(364);
   });
 });
